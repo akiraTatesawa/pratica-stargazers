ca343a892a01ef96b326d47e414ce4bf7bcd34f1
Arthur Akira <arthurak10.at@gmail.com>
2022-08-31 20:45:06 -0300

    fix: fix ranking query

--- src/repositories/rankingRepository.ts
+++ src/repositories/rankingRepository.ts
@@ -2,7 +2,7 @@ import { connection } from "../databases/postgres/connection";
 
 export async function getRanking() {
   const result = await connection.query(`
-  SELECT fighters.username, fighters.wins, fighters.losses, fighters.draws FROM fighters ORDER BY fighters.wins DESC
+  SELECT fighters.username, fighters.wins, fighters.losses, fighters.draws FROM fighters ORDER BY wins DESC, draws DESC
   `);
 
   return result.rows;
